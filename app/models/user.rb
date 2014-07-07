88898bd1874e89f295628ca4a27f3784f850f2f7
Wei-Meng Lee <weimeng@staticfusion.org>
2014-07-07 12:40:12 +0800

    Add validations and adjust rails_admin fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,29 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, on: :create
+  validates :password, length: { minimum: 8 }, if: "password.present?"
+
+  rails_admin do
+    list do
+      configure :crypted_password do
+        hide
+      end
+      configure :salt do
+        hide
+      end
+    end
+
+    edit do
+      configure :crypted_password do
+        hide
+      end
+      configure :salt do
+        hide
+      end
+      field :email
+      field :password
+    end
+  end
 end
