75fa87952b4b0f6a349581954c04e592d5994da3
Wei-Meng Lee <weimeng@staticfusion.org>
2014-07-17 19:05:24 +0800

    Add WYSIWYG editor to speakers

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -15,4 +15,22 @@ class Speaker < ActiveRecord::Base
       "#{self.first_name} #{self.last_name}"
     end
   end
+
+  rails_admin do
+    edit do
+      field :slug
+      field :first_name
+      field :last_name
+      field :asian_name
+      field :position
+      field :company
+      field :website_link
+      field :linkedin_link
+      field :twitter_link
+      field :bio, :wysihtml5
+      field :photo
+      field :published
+      field :programmes
+    end
+  end
 end
