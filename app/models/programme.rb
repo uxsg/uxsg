3c12866333990e1a0ab8224f11c6e1eb9111db95
Wei-Meng Lee <weimeng@staticfusion.org>
2014-08-02 11:20:28 +0800

    Add "tour" category

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -8,7 +8,8 @@ class Programme < ActiveRecord::Base
   enum category: { misc:                       0,
                    keynote:                    1,
                    workshop:                   2,
-                   "lightning presentation" => 3 }
+                   tour:                       3,
+                   "lightning presentation" => 4 }
 
   scope :day_1, -> { where(day: 1) }
   scope :day_2, -> { where(day: 2) }
