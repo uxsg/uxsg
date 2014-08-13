fa23d330faf968417154d5c4155d83ad633400c1
Wei-Meng Lee <weimeng@staticfusion.org>
2014-08-13 18:18:57 +0800

    Initial work on programmatic programme

--- app/controllers/programmes_controller.rb
+++ app/controllers/programmes_controller.rb
@@ -1,9 +1,9 @@
 class ProgrammesController < ApplicationController
   def index
     @programmes = Programme.order(happens_at: :asc).all
-    # @day_1 = Programme.day_1.order(happens_at: :asc)
-    # @day_2 = Programme.day_2.order(happens_at: :asc)
-    # @day_3 = Programme.day_3.order(happens_at: :asc)
+    @day_1 = Programme.day_1.order(happens_at: :asc).order(industry_track_id: :desc)
+    @day_2 = Programme.day_2.order(happens_at: :asc).order(industry_track_id: :desc)
+    @day_3 = Programme.day_3.order(happens_at: :asc).order(industry_track_id: :desc)
   end
 
   def show
